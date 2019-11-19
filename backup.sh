d605f08e818395504a366545127aecbfc62e7d22
Jus de Patate_ <jusdepatate@protonmail.com>
2019-11-19 21:46:29 +0100

    gives a better .tar.xz file
    I had to use it recently so I had to optimise the code and the archive.
    tbd:
     - support arguments
     - paths in vars

--- backup.sh
+++ backup.sh
@@ -2,53 +2,43 @@ echo "[START] Backup started on $(date)" > backup.log
 echo "Starting backup"
 
 if [ ! "$(whoami)" = "root" ]; then
-     echo "[ERROR] Execute as Super User"
-     echo "[END] Backup failed on $(date)" >> backup.log
-     exit 1
+ echo "[ERROR] Execute as Super User"
+ echo "[END] Backup failed on $(date)" >> backup.log
+ exit 1
 fi
 
 echo "[mkdir] Creating required folders"
-mkdir ~/backup &>~/backup.log
-mkdir ~/backup/{letsencrypt,a2vhost,a2files,apt,ts,etc} &>>~/backup.log
-
-echo "[cp] Backuping Let's Encrypt certificates"
-cp -R /etc/letsencrypt/live/* ~/backup/letsencrypt &>>~/backup.log
-
-echo "[cp] Backuping Apache2 vhost configuration"
-cp -R /etc/apache2/sites-available/* ~/backup/a2vhost &>>~/backup.log
+mkdir ~/backup 2>>~/backup.log
+mkdir ~/backup/{a2files,apt,etc,minecraft} 2>>~/backup.log
 
 echo "[cp] Backuping Apache2 files"
-cp -R /var/www/* ~/backup/a2files &>>~/backup.log
-
-echo "[tar] Compressing Apache2 files"
-cd ~/backup/ && tar -cvpJf a2files.tar.xz ~/backup/a2files/ &>>~/backup.log
-rm -R ~/backup/a2files/ &>>~/backup.log
+cp -R /var/www/* ~/backup/a2files 2>>~/backup.log
 
 echo "[cp] Backuping /etc"
-cp -R /etc/* ~/backup/etc  &>>~/backup.log
-
-echo "[tar] Compressing /etc"
-cd ~/backup/ && tar -cvpJf etc.tar.xz ~/backup/etc/ &>>~/backup.log
-rm -R ~/backup/etc/ &>>~/backup.log
+cp -R /etc/* ~/backup/etc  2>>~/backup.log
 
 echo "[apt] Backuping software installed thru APT"
 apt list 2>>~/backup.log | grep "installed" > ~/backup/apt/installed
 
-echo "[cp] Backuping TeamSpeak3 files"
-cp -R ~teamspeak/* ~/backup/ts/ &>>~/backup.log
+#echo "[cp] Backuping TeamSpeak3 files"
+#cp -R ~teamspeak/* ~/backup/ts/ 2>>~/backup.log
 
 echo "[mysqldump] Backuping MySQL databases"
 su backup -c "mysqldump -A -Y -C -f -u backup" > ~/backup/mariadb.sql 2>>~/backup.log
 
+#echo "[cp] Backuping MC"
+#cp -R ~root/minecraft/* ~/backup/minecraft/ 2>>~/backup.log
+
 echo "[tar] Compressing backup"
-cd ~ && sudo tar -cvpJf backup.tar.xz ~/backup &>>~/backup.log
+cd ~ && sudo tar -cvpJf backup.tar.xz ~/backup 2>>~/backup.log
 rm -r ~/backup &>>~/backup.log
 
-echo "[mv] Putting backup on Nextcloud"
-rm /var/www/nextcloud/data/jusdepatate/files/backup.tar.xz
-mv ~/backup.tar.xz /var/www/nextcloud/data/jusdepatate/files/backup.tar.xz
+#echo "[mv] Putting backup on Nextcloud"
+#rm /var/www/nextcloud/data/jusdepatate/files/backup.tar.xz
+#mv ~/backup.tar.xz /var/www/nextcloud/data/jusdepatate/files/backup.tar.xz
 
-echo "[info] Backup log in file backup.log"
+echo "[info] Backup log in file ~/backup.log"
+echo "[info] Backup is in file backup.tar.xz"
 echo "End of Backup"
 
 echo "[END] Backup ended on $(date)" >> ~/backup.log
